11286559b160e18b28c535a5f8dddbc3fa450f03
DenysBerezniuk <49430756+FranklinMar@users.noreply.github.com>
2023-04-15 17:20:32 +0300

    Refactor Result displaying

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -42,9 +42,10 @@ namespace Lab2
             Network NeuralNetwork = new (Sigmoid.Instance, Inputs, Outputs, 3);
             //NeuralNetwork.ShowResult = true;
             NeuralNetwork.Propagate(new List<double>() { Output.Value }, 0.1, 0.1, 1E-18);
-            for (int epoch = 0; epoch < 10; epoch++)
+            double Epochs = 100;
+            for (int epoch = 0; epoch < Epochs; epoch++)
             {
-                Console.WriteLine((epoch / 10.0) + "%");
+                Console.WriteLine((epoch / Epochs * 100) + "%");
                 foreach (KeyValuePair<string, List<int[]>> DataPair in ConvolutedDatasets)
                 {
                     List<double> ResultList = new() { Double.Parse(DataPair.Key) };
@@ -61,6 +62,20 @@ namespace Lab2
                     }
                 }
             }
+            Console.WriteLine("LEARNING RESULT: ");
+            DatasetManager.Shuffle(Datasets["1"]);
+            Console.WriteLine("'1': ");
+            DisplayArray(Datasets["1"][0]);
+            Console.WriteLine($"Result: {NeuralNetwork.Calculate(Array.ConvertAll<int, double>(DatasetManager.FlattenArray(ConvolutionLayer.MaxPool(ConvolutionLayer.Convolute(Datasets["1"][0]))), x => x))[0]}");
+            DatasetManager.Shuffle(Datasets["2"]);
+            Console.WriteLine("'2': ");
+            DisplayArray(Datasets["2"][0]);
+            Console.WriteLine($"Result: {NeuralNetwork.Calculate(Array.ConvertAll<int, double>(DatasetManager.FlattenArray(ConvolutionLayer.MaxPool(ConvolutionLayer.Convolute(Datasets["2"][0]))), x => x))[0]}");
+            DatasetManager.Shuffle(Datasets["3"]);
+            Console.WriteLine("'3': ");
+            DisplayArray(Datasets["3"][0]);
+            Console.WriteLine($"Result: {NeuralNetwork.Calculate(Array.ConvertAll<int, double>(DatasetManager.FlattenArray(ConvolutionLayer.MaxPool(ConvolutionLayer.Convolute(Datasets["3"][0]))), x => x))[0]}");
+
         }
 
         public static void DisplayArray(int [][] Dataset)
